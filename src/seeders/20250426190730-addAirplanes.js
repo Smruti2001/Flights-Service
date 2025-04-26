4acd0ae15da4d4a76b6a771b5a3baf3438920420
Smruti Mahapatra <getsmruti03@gmail.com>
2025-04-27 00:52:23 +0530

    Added seeders

---
+++ src/seeders/20250426190730-addAirplanes.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const { Op } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('Airplanes', [
+      {
+        modelNumber: 'Boeing 747-8',
+        capacity: 600,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        modelNumber: 'Airbus A340-300',
+        capacity: 295,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ])
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    await queryInterface.bulkDelete('Airplanes', { [Op.or]: [{ modelNumber: 'Boeing 747-8' }, { modelNumber: 'Airbus A340-300' }] })
+  }
+};
