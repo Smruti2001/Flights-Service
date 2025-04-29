957ce5b5a4b31dba99575f69944da218446d6b9d
Smruti Mahapatra <getsmruti03@gmail.com>
2025-04-30 01:14:26 +0530

    Addeed the validation to check the incoming date and airport code in flightMiddleware

--- src/middlewares/flightMiddlewares.js
+++ src/middlewares/flightMiddlewares.js
@@ -2,6 +2,7 @@ const { StatusCodes } = require('http-status-codes');
 
 const AppError = require('../utils/errors/AppError');
 const { ErrorResponse } = require('../utils/common');
+const compareDateTime = require('../utils/helpers/dateTimeHelper');
 
 function validateCreateRequest(req, res, next) {
     if(!req.body.flightNumber) {
@@ -49,6 +50,18 @@ function validateCreateRequest(req, res, next) {
     if(!req.body.arrivalTime) {
         ErrorResponse.message = 'Something went wrong while creating flight';
         ErrorResponse.error = new AppError(['arrivalTime not found in the incoming request in the required form'], StatusCodes.BAD_REQUEST);
+        return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+    }
+
+    if(!compareDateTime(req.body.arrivalTime, req.body.departureTime)) {
+        ErrorResponse.message = 'Something went wrong while creating flight';
+        ErrorResponse.error = new AppError(['arrivalTime should be greater than the departureTime'], StatusCodes.BAD_REQUEST);
+        return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+    }
+
+    if(req.body.departureAirportId == req.body.arrivalAirportId) {
+        ErrorResponse.message = 'Something went wrong while creating flight';
+        ErrorResponse.error = new AppError(['departureAirportId can not be same as arrivalAirportId'], StatusCodes.BAD_REQUEST);
         return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
     }
 
---
+++ src/utils/helpers/dateTimeHelper.js
@@ -0,0 +1,7 @@
+function compareDateTime(dateTime1, dateTime2) {
+    const dt1 = new Date(dateTime1);
+    const dt2 = new Date(dateTime2);
+    return dt1.getTime() > dt2.getTime();
+}
+
+module.exports = compareDateTime
