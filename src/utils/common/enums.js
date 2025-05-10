b91a64a658d2c3a08d16fc6797f5c84ae5a370c2
Smruti Mahapatra <getsmruti03@gmail.com>
2025-05-11 01:47:56 +0530

    Added the seats model

---
+++ src/utils/common/enums.js
@@ -0,0 +1,10 @@
+const SEAT_TYPE = {
+    ECONOMY: 'economy',
+    PREMIUM_ECONOMY: 'premium-economy',
+    BUSSINESS: 'bussiness',
+    FIRST_CLASS: 'first-class'
+}
+
+module.exports = {
+    SEAT_TYPE
+}
